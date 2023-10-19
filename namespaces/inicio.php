8c31e7f90a14bb6737256fcd2f25e2f4c1a1e4e9
OussamaAtifii <oussamaati03@gmail.com>
2023-10-19 17:12:03 +0200

    add autoload with namespaces

--- namespaces/inicio.php
+++ namespaces/inicio.php
@@ -1,8 +1,12 @@
 <?php
+spl_autoload_register(function ($nombre) {
+    $nombre = str_replace("\\", "/", $nombre) . ".php";
+    require $nombre;
+});
 
-require "./src/backend/Users.php";
-require "./src/frontend/Users.php";
-require "./src/Admin.php";
+// require "./src/backend/Users.php";
+// require "./src/frontend/Users.php";
+// require "./src/Admin.php";
 
 use src\Admin;
 use src\backend\Users;
@@ -11,3 +15,5 @@ use src\frontend\Users as FrontendUsers;
 $manolo = new Users("Oussama");
 $paco = new FrontendUsers("Paco");
 $admin = new Admin();
+echo "<br>";
+echo $manolo::class;
