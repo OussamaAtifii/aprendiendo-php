3f966f23ec21ecfef9c5473088c26ecbb1b599f5
OussamaAtifii <oussamaati03@gmail.com>
2023-10-25 19:35:43 +0200

    add table to display user data

--- crud-23-10-23/public/index.php
+++ crud-23-10-23/public/index.php
@@ -5,6 +5,7 @@ use Src\Models\Usuario;
 require_once __DIR__ . "/../vendor/autoload.php";
 
 Usuario::crearRegistrosAleatorios(120);
+$todos = Usuario::read();
 
 ?>
 <!DOCTYPE html>
@@ -12,11 +13,74 @@ Usuario::crearRegistrosAleatorios(120);
 
 <head>
     <meta charset="UTF-8">
+    <!-- TAILWIND -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- SWEETALERT -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Document</title>
 </head>
 
 <body>
+    <h1 class="text-center text-xl mt-5">Listado de Usuarios</h1>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg w-2/3 m-5 mx-auto">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Nombre
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Apellido
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Email
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Provincia
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Perfil
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($todos as $usuario) {
+                    echo <<<TXT
+                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {$usuario->nombre}
+                    </th>
+                    <td class="px-6 py-4">
+                        {$usuario->apellidos}
+                    </td>
+                    <td class="px-6 py-4">
+                        Apple MacBook Pro 17"
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop
+                    </td>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    </td>
+                </tr>
+                TXT;
+                }
+                ?>
+
+            </tbody>
+        </table>
+    </div>
 
 </body>
 
